d6429224c6f89fcc5642a921236f18bd1e08b552
kannr <echo_follow_heart@163.com>
2018-01-20 20:57:47 +0800

    init

---
+++ src/main/java/com.thouseye/Sandbox.java
@@ -0,0 +1,4 @@
+package com.thouseye;
+
+public class Sandbox {
+}
---
+++ src/main/java/com.thouseye/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.thouseye.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ArticleController {
+
+
+}
